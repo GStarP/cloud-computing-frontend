3dcdc09f6b16d0804712d3bb08fc3a5aac6ce9ac
GStarP <171250605@smail.nju.edu.cn>
2019-10-04 22:58:53 +0800

    feat: 完成简易menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>CLOUD-COMPUTING</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-btn dark href="https://github.com/GStarP/cloud-computing-frontend">
+        GITHUB
+        <v-icon class="ml-2">mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-content>
       <router-view/>
     </v-content>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,20 +1,18 @@
 <template>
-  <v-layout justify-space-between wrap>
-    <v-flex md3>
-      <v-card>
-
-      </v-card>
-    </v-flex>
-    <v-flex md3>
-      <v-card>
-
-      </v-card>
-    </v-flex>
-    <v-flex md3>
-      <v-card>
-
-      </v-card>
-    </v-flex>
+  <v-layout id="menu-container">
+    <div id="menu">
+      <v-responsive
+        v-for="(item, i) of menu"
+        :key="i"
+        :aspect-ratio="1"
+        class="menu-item"
+      >
+        <v-card class="menu-item-card" :to="item.path">
+          <v-icon class="menu-item-card-img mb-3" large>{{ item.img }}</v-icon>
+          <div class="menu-item-card-text">{{ item.name }}</div>
+        </v-card>
+      </v-responsive>
+    </div>
   </v-layout>
 </template>
 
@@ -25,18 +23,70 @@ export default {
       menu: [
         {
           name: '流计算',
-          path: '/streaming'
+          path: '/home/streaming',
+          img: 'mdi-weather-windy'
         },
         {
           name: '图计算',
-          path: '/graph-x'
+          path: '/home/graph-x',
+          img: 'mdi-merge'
         },
         {
           name: '机器学习',
-          path: '/machine-learning'
+          path: '/home/machine-learning',
+          img: 'mdi-robot'
         }
       ]
     };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+#menu-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+#menu {
+  display: flex;
+  flex-direction: row;
+  width: $menuWidth;
+  margin-bottom: $topBarHeight;
+}
+.menu-item {
+  width: $menuItemWidth;
+  border-radius: 10px;
+  box-shadow: 0 1px 2px 0 rgba(60,64,67,.08), 0 1px 3px 2px rgba(60,64,67,.16);
+  &:nth-child(2) {
+    margin: 0 $menuItemMargin;
+  }
+  @keyframes wave {
+    100% {
+      transform: translate(-8px, -8px);
+    }
+  }
+  &:hover {
+    animation-name: wave;
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards;
+  }
+}
+.menu-item-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+.menu-item-card-img {
+  color: black;
+}
+.menu-item-card-text {
+  color: black;
+  font-size: $cardFontSize;
+}
+</style>
